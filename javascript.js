fef6f7002c6914acaf75cc755d3a72acc4bebc6e
John Contreras <johndevproject@gmail.com>
2024-05-23 17:42:57 +0800

    Final commit
    added div height and width. created mouseover func and button click event. added if else for newprompt

--- javascript.js
+++ javascript.js
@@ -1,55 +1,73 @@
-// first commit //
-
-// Grid divs here //
-
 const mainDiv = document.createElement("div");
 mainDiv.classList.add("main-div");
 const container = document.querySelector(".container");
 container.appendChild(mainDiv);
-let numberOfGrid = 16;
+
 const gridDiv = document.createElement("div");
 gridDiv.classList.add("grid-div");
-gridDiv.style.height = 600/numberOfGrid + "px";
-gridDiv.style.width = 600/numberOfGrid + "px";
 
+gridDiv.style.height = "37.5px";
+gridDiv.style.width = "37.5px";
 
+for (i = 1; i <= 256; i++) {
+  mainDiv.appendChild(gridDiv.cloneNode(true));
+}
 
-
-for ( i = 1; i<=(numberOfGrid ** 2); i++ ) {
-mainDiv.appendChild(gridDiv.cloneNode(true));
-  }
-
-
-// Hover even here //
-
-const gridDivEvent = document.querySelectorAll(".grid-div");
-
-gridDivEvent.forEach((grid) => {
-
-grid.addEventListener("mouseover", (event) => {
-  event.target.style.backgroundColor= "#27374D";
-
-  
-setTimeout(() => {
-      event.target.style.backgroundColor = "#526D82";
-    }, 300);
-  },
-  false,
-);
-
+function mouseOver() {
+  const gridDivEvent = document.querySelectorAll(".grid-div");
+  gridDivEvent.forEach((grid) => {
+    grid.addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = "#481E14";
+      //commit: added progressing darkening bg color //
+      event.target.style.opacity =
+        (parseFloat(event.target.style.opacity) || 0) + 0.2;
     });
-
-// Popup button and new grid here //
-
-document.querySelector("button").addEventListener("click", () => {
-  let newGrid = prompt("Enter your preferred grid size (1-100 only)", 16);
-  
-  if (newGrid > 100) {
-    alert("Invalid grid size. Enter 1-100 only. Please try again");
-    prompt("Enter your preferred grid size (1-100 only)", 16);
-    } 
-  
-  alert("Setting up your preferred grid size... Press OK");
-
-
   });
+}
+
+mouseOver();
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // commit: add if else for buttons //
+    const selectedButton = button.id;
+
+    if (selectedButton === "new-grid") {
+      let newGrid = prompt("Enter your preferred grid size (1-100 only)");
+
+      if ((newGrid > 100 || newGrid == 0) && newGrid != null) {
+        alert("Invalid grid size. Enter 1-100 only.");
+
+        let newPrompt = prompt("Enter your preferred grid size (1-100 only)");
+
+        if ((newPrompt > 100 || newPrompt == 0) && newPrompt != null) {
+          alert("Invalid grid size. Try Again!");
+        } else if (newPrompt != null) {
+          mainDiv.innerHTML = " ";
+          gridDiv.style.height = 600 / newPrompt + "px";
+          gridDiv.style.width = 600 / newPrompt + "px";
+        }
+      } else if (newGrid != null) {
+        mainDiv.innerHTML = " ";
+        gridDiv.style.height = 600 / newGrid + "px";
+        gridDiv.style.width = 600 / newGrid + "px";
+      }
+
+      for (i = 1; i <= newGrid ** 2; i++) {
+        mainDiv.appendChild(gridDiv.cloneNode(true));
+      }
+
+      mouseOver();
+    } else {
+      const gridSize = 600 / parseFloat(gridDiv.style.height);
+      mainDiv.innerHTML = " ";
+
+      for (i = 1; i <= gridSize ** 2; i++) {
+        mainDiv.appendChild(gridDiv.cloneNode(true));
+      }
+
+      mouseOver();
+    }
+  });
+});
